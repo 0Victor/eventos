1600928c18c24a08c71fa1c476f6cdf2ff284253
0Victor <135348712+0Victor@users.noreply.github.com>
2023-10-06 11:18:27 -0300

    Eventos

---
+++ evento.js
@@ -0,0 +1,16 @@
+function passou(){
+    document.querySelector('#card').innerText = "Passou o Mouse"
+}
+
+function mouseOut(){
+    document.querySelector('#card').innerText = "Tirou o Mouse"
+}
+
+function clique(){
+    document.querySelector('#card').innerText = "Clicou"
+}
+
+
+// function clicou(){
+//     console.log
+// }
